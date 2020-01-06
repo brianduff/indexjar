50044694e3df18da5b25197d6a06ba3efe66865d
Brian Duff <bduff@fb.com>
2020-01-06 15:02:22 -0800

    Initial revision

---
+++ java/org/dubh/indexjar/IndexJar.java
@@ -0,0 +1,67 @@
+package org.dubh.indexjar;
+
+import java.util.*;
+import java.nio.file.*;
+import java.io.*;
+import java.util.stream.*;
+import com.google.common.base.Splitter;
+import com.google.common.collect.Multimap;
+import com.google.common.collect.HashMultimap;
+import java.nio.charset.StandardCharsets;
+import java.util.zip.*;
+
+public class IndexJar {
+  private static final Splitter PATH_SPLITTER = Splitter.on(":");
+
+  private final List<String> entries;
+
+  IndexJar(List<String> entries) {
+    this.entries = entries;
+  }
+
+  private void generate() throws IOException {
+    Multimap<String, String> pathToMatchingJars = HashMultimap.create();
+    for (String jarFile : entries) {
+      File file = new File(jarFile);
+      if (file.isFile()) {
+        Set<String> dirs = getDirectoriesContainingClasses(new ZipFile(file));
+        for (String dir : dirs) {
+          pathToMatchingJars.put(dir, jarFile);
+        }
+      }
+    }
+  }
+
+  private Set<String> getDirectoriesContainingClasses(ZipFile zipFile) {
+    Set<String> directories = new HashSet<>();
+    Enumeration<? extends ZipEntry> entries = zipFile.entries();
+    while (entries.hasMoreElements()) {
+      ZipEntry entry = entries.nextElement();
+      String name = entry.getName();
+      if (name.endsWith(".class")) {
+        String dirName = entry.getName().substring(0, name.lastIndexOf('/'));
+        directories.add(dirName);
+      }
+    }
+    return directories;
+  }
+
+  public static void main(String[] args) throws Exception {
+    // Support reading classpath entries from an @ file.
+    List<String> allEntries = new ArrayList<>(1000);
+    for (String arg : args) {
+      if (arg.length() > 1 && arg.charAt(0) == '@') {
+        loadEntriesFromFile(Paths.get(arg.substring(1)), allEntries);
+      } else {
+        allEntries.addAll(PATH_SPLITTER.splitToList(arg));
+      }
+    }
+    new IndexJar(allEntries).generate();
+  }
+
+  private static void loadEntriesFromFile(Path file, List<String> allEntries) throws IOException {
+    try (Stream<String> stream = Files.lines(file, StandardCharsets.UTF_8)) {
+      stream.forEach(s -> allEntries.addAll(PATH_SPLITTER.splitToList(s)));
+    }
+  }
+}
